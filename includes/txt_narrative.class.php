97ec55989cf887aab239876de93d7c467063acff
adam <adam@cangjie.info>
2020-04-14 08:48:23 -0400

    corrcted name of narrative class file.

---
+++ includes/txt_narrative.class.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('txt_sentence.class.php');
+
+class TxtNarrative {
+  public $id;
+  public $subcollection_id;
+  public $name_en;
+  public $name_zh;
+  public $number;
+  public $sentences = array();
+
+  public function appendSentence(TxtSentence $sentence) {
+    $this->sentences[] = $sentence;
+  }
+
+  public function getLength() {
+    return count($this->sentences);
+  }
+
+  public function getGraphCount() {
+    $count = 0;
+    foreach($this->sentences as $sentence) {
+      $count += $sentence->getLength();
+    }
+    return $count;
+  }
+
+  public function toString() {
+    $str = '';
+    foreach($this->sentences as $sentence) {
+      $str .= $sentence->toString();
+    }
+    return $str;
+  }
+}
+
+
