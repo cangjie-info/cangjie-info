a41783b4c9b4dcccf4f339be8b25be2d36799533
adam <adam@cangjie.info>
2020-04-14 01:45:22 -0400

    extended narratives and sentences functionality

--- sentence/index.php
+++ sentence/index.php
@@ -1,19 +1,31 @@
 <?php 
-require('../includes/all_php.php');
-require('../includes/db.php');
+
+require_once('../includes/all_php.php');
+require_once('../includes/txt_sentence.class.php');
+require_once('../includes/db.php');
 
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-$qrySentence = 'SELECT graph, FROM txt_sentences ' .
-  'INNER JOIN inscr_graphs ON txt_sentences.id=sentence_id ' .
-  'WHERE txt_sentences.id=:id ' .
-  'ORDER BY number_sentence;';
-  
+$qrySentence = 'SELECT * FROM txt_sentences WHERE id=:id;';
 $stmtSentence = $db->prepare($qrySentence);
 $stmtSentence->bindValue(':id', $id);
 $stmtSentence->execute();
-$sentence = $stmtSentence->fetchAll();
-$stmtSentence->closeCursor();
+$sentence = $stmtSentence->fetchObject('TxtSentence');
 
-var_dump($sentence);
+$qryGraphs = 'SELECT inscr_id, number_inscr, markup, punc, sentence_id, number_sentence, graph ' .
+  'FROM txt_sentences ' .
+  'INNER JOIN inscr_graphs ON txt_sentences.id=sentence_id ' .
+  'WHERE txt_sentences.id=:id ' .
+  'ORDER BY number_sentence;';
+$stmtGraphs = $db->prepare($qryGraphs);
+$stmtGraphs->bindValue(':id', $id);
+$stmtGraphs->execute();
+while($graph = $stmtGraphs->fetchObject('InscrGraph')) {
+  $sentence->appendGraph($graph);
+}
+$stmtGraphs->closeCursor();
+
+echo $sentence->toString();
+echo '<a href="../narrative/?id=' . $sentence->narrative_id . '" >LINK TO NARRATIVE</a>';
+
 ?>
