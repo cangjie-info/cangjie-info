a632feed4e3e512dbb387395d558a77672e6d299
adam <adam@cangjie.info>
2020-04-22 21:07:57 -0400

    further work on pagination.

--- pagination/index.php
+++ pagination/index.php
@@ -8,6 +8,8 @@ require_once('../includes/db.php');
 $collection_id = filter_input(INPUT_POST, 'collection_id', FILTER_SANITIZE_NUMBER_INT);
 $pub_id = filter_input(INPUT_POST, 'pub_id', FILTER_SANITIZE_NUMBER_INT);
 
+// if the collection and publication are not known,
+// we need to present the choice from those available to the user.
 if(!isset($collection_id) or !isset($pub_id)) {
   $qry = 'SELECT * FROM pubs;';
   $stmt = $db->prepare($qry);
@@ -22,25 +24,23 @@ if(!isset($collection_id) or !isset($pub_id)) {
   require_once('choose_pub.html.php');
   exit();
 } 
-else {
-  echo "collection_id = " . $collection_id . ", pub_id = " . $pub_id;
-}
-
-// get all graphs in collection with their ids and row numbers by collection
-// order
-$qry = 'SELECT graph, inscr_graphs.id AS id ' .
+// Get all graphs in collection with their ids ordered within collection
+$qry = 'SELECT graph, inscr_graphs.id AS id, pub_page, interpolated ' .
   'FROM inscr_graphs ' .
   'INNER JOIN txt_sentences ON txt_sentences.id = inscr_graphs.sentence_id ' .
   'INNER JOIN txt_narratives ON txt_narratives.id = txt_sentences.narrative_id ' .
   'INNER JOIN txt_subcollections ON txt_subcollections.id = txt_narratives.subcollection_id ' .
+  'LEFT JOIN pubs_graph_page ON inscr_graphs.id=pubs_graph_page.graph_id ' .
   'WHERE collection_id=:collection_id ' .
+  'AND (pubs_graph_page.pub_id=:pub_id OR pubs_graph_page.pub_id IS NULL) ' .
   'ORDER BY txt_subcollections.number, txt_narratives.number, txt_sentences.number, number_sentence;';
 $stmt = $db->prepare($qry);
 $stmt->bindValue(':collection_id', $collection_id);
+$stmt->bindValue(':pub_id', $pub_id);
 $stmt->execute();
 $collection_graphs_ids = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// figure out if the pagination mappings are already there
+// figure out if the pagination mappings are already there.
 // if not, figure out how many pages there are and add those to the pagination
 // table, with interpolated graph_id.
 
@@ -49,18 +49,22 @@ $qry = 'SELECT name, page_offset, pubs_graph_page.id as id, graph_id, pub_page, 
   'RIGHT JOIN pubs ON pubs.id = pubs_graph_page.pub_id ' .
   'WHERE pubs.id = :pub_id ' .
   'ORDER BY pub_page;';
+//TODO currently the query will not handle the case where a publication maps to 
+//more than one collection. Fix this, by joining through txt hierarchy and adding
+//collection id to where clause. Possibly do this in a single query with
+//previous query which is similar.
 $stmt = $db->prepare($qry);
 $stmt->bindValue(':pub_id', $pub_id);
 $stmt->execute();
 $pagination = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if($pagination === false) {
+if($pagination === false) { // if query fails...
   echo '$pagination === false at line 41';
   exit();
 }
-if(is_null($pagination[0]['id'])) {
+if(is_null($pagination[0]['id'])) { // if no pagination exsits for this publication...
   $pub_name = $pagination[0]['name'];
   echo "No pagination exists for pub_id = $pub_name.\n";
-  $page_count = check_pub_dir($pub_name);
+  $page_count = check_pub_dir($pub_name); // function verifies that publication img files are correct.
   if($page_count === false) {
     echo "check_pub_dir() returned false.";
     exit();
@@ -81,16 +85,51 @@ if(is_null($pagination[0]['id'])) {
   echo $qry . "\n";
   $db->exec($qry);
 }
-else {
-  var_dump($pagination);
+else { // pagination table already exists for this publication
+  $offset = $pagination[0]['page_offset'];
+  $graph_span = filter_input(INPUT_POST, 'graph_span', FILTER_SANITIZE_NUMBER_INT);
+  if(!isset($gaph_span)) {
+    $graph_span = 100;
+  }
+  // get graph number in collection ordering (must be in range) or =1
+  $graph_number = filter_input(INPUT_POST, 'graph_number', FILTER_SANITIZE_NUMBER_INT);
+  if(!isset($graph_number) || $graph_number < 0) {
+    $graph_number = 0;
+    //todo set graph number accoring to page
+  }
+  elseif ($graph_number >= count($collection_graphs_ids)) {
+    $graph_number = count($collection_graphs_ids) - $graph_span;
+  }
+  // get page number (must be in range) or =1 + offset
+  $page_number = filter_input(INPUT_POST, 'page_number', FILTER_SANITIZE_NUMBER_INT);
+  if(!isset($page_number) || $page_number - $offset < 1) {
+    $page_number = 1 + $offset;
+    //todo set page number according to graph
+  }
+  elseif($page_number >= count($pagination) + $offset) {
+    $page_number = count($pagination) + $offset - 1;
+  }
+  // get graphs in range
+  $str_graphs = '';
+  for($n = $graph_number; $n < $graph_number + $graph_span; $n++) {
+    $str_graphs .= $collection_graphs_ids[$n]['graph'];
+    // insert page markers into graph sequence
+    if(isset($collection_graphs_ids[$n]['pub_page'])) {
+      $marker = "[" . $collection_graphs_ids[$n]['pub_page'];
+      if($collection_graphs_ids[$n]['interpolated'] == 1) {
+        $marker .= 'i';
+      }
+      $marker .= ']';
+      $str_graphs .= $marker;
+    }
+  }
+  echo $str_graphs;
+  require_once('paginator.html.php');
+
+  // display page
 }
 
 /*
-//
-// Allow setting number of graphs at a time.
-//
-// set page to first page.
-// set graph to first graph in collection.
 //
 // display page
 // display graphs (include marker for existing page breaks) by printing 
---
+++ pagination/paginator.html.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+    </title>
+  </head>
+  <body>
+    <p>Paginator:
+<?php /*
+    <!-- previous page button -->
+    <form action="./" method="get">
+      <input type="hidden" 
+             name="name" 
+             value="<?php echo htmlspecialchars($name); ?>" 
+             />
+      <button name="page" type="submit" 
+                          value="<?php echo $page - 1; ?>" >prev page</button>
+      <button name="page" type="submit" 
+                          value="<?php echo $page + 1; ?>" >next page</button>
+    </form>
+    <form>
+      <input type="submit" value="jump to page: " >
+      <input type="text" name="page">
+      <input type="hidden" 
+             name="name" 
+             value="<?php echo htmlspecialchars($name); ?>" 
+             />
+    </form>
+    </p>
+    <p>
+    <?php echo $bib_string . ' p. ' . $page; ?>
+    </p>
+    <img style="max-width: 100%" src="<?php echo $file_name; ?>" />
+ */
+?>
+  </body>
+</html>
