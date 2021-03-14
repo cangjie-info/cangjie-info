044d87a568f47bd8f9ca50578968a7d5e01c32c8
adam <adam@cangjie.info>
2021-03-13 23:53:42 -0500

    tidying up.

--- includes/db.php
+++ includes/db.php
@@ -2,22 +2,12 @@
 
 require('../../credentials.php');
 
-// adds new collection with name = $name_zh
-// and returns new id
-// if it does not already exist.
-// if it does already exist, just return the id. 
-/* function add_collection($name_zh, $db) {
-	$qry_str = 'INSERT INTO txt_collections ' .
-		'(
-
- */
-
 try {
 	$db = new PDO($dsn, $username, $password);
 }
 catch(PDOException $e) {
 	$error_message = $e->getMessage();
-	include('../includes/db_error.php');
+	include('../includes/error.php');
 	exit();
 }
 ?>
