e404461b407b54cee428d0695e89eed0d1f74516
adam <adam@cangjie.info>
2021-03-23 20:27:17 -0400

    Begun Context page.

---
+++ context/context.html.php
@@ -0,0 +1,19 @@
+<h1>Archeological context: 
+<?php 
+echo $context['name'] . ' (' . $context['context_type'] . ')';
+?>
+</h1>
+<!-- CONTEXT DETAILS -->
+<p><?php echo $context['description'];?></p>
+<p>Date: <?php echo $context['date_early'] . ' - ' . $context['date_late']; ?></p>
+
+
+<!-- NEXT CONTEXT -->
+<!-- PREV CONTEXT -->
+<!-- EDIT CONTEXT DETAILS -->
+<!-- ARCH_OBJECTS IN CONTEXT -->
+<!-- ADD NEW ARCH_OBJECT -->
+	<!-- create inscr_object to go with it by default -->
+<!-- ASSOCIATED INSCR_OBJECTS -->
+<!-- ASSOCIATED INSCR_SURFS -->
+<!-- ASSOCIATED INSCRS -->
---
+++ context/index.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once('../includes/all_php.php');
+require_once('../includes/db.php');
+
+// get action from $_POST variable
+$action = filter_input(INPUT_POST, 'action');
+// default to "display"
+if($action === NULL){
+	$action = "display";
+}
+
+/* 
+
+// DELETE ACTION
+if($action == "delete"){
+	// do deleting here
+	header('location: .'); // action will default to "display" on reload. 
+	exit;
+}
+
+// ADD ACTION
+else if($action == "add"){
+	trim_POST();
+	// get data fields
+	// validate input
+	if (not valid) {
+		$error_message = "Invalid excavation data.";
+		include('../includes/error.php');
+		exit;
+	}
+	else {
+		$query = 'INSERT INTO etc. etc.';
+		header('location: .');
+		exit;
+	}
+}
+
+ */
+
+// DISPLAY ACTION
+if($action == "display"){
+	$id = filter_input(INPUT_GET, 'id');
+	if($id === NULL){
+		$error_message = 'Need an integer id for context.';
+		include('../includes/error.php');
+		exit;
+	}
+	else {
+		$qry = 'SELECT * FROM arch_contexts '
+			. 'WHERE id = :id;';
+		$stmt = $db->prepare($qry);
+		$stmt->bindValue(':id', $id);
+		$stmt->execute();
+		$context = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		$stmt->closeCursor();
+	}
+	if(count($context) === 0){
+		$error_message = 'No context with that id exists.';
+		include('../includes/error.php');
+		exit;
+	}
+	else {
+		$context = $context[0];
+		$qry = 'SELECT * FROM arch_objects '
+			. 'WHERE arch_context_id = :id;';
+		$stmt = $db->prepare($qry);
+		$stmt->bindValue(':id', $id);
+		$stmt->execute();
+		$objects = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		$stmt->closeCursor();
+		require_once('../includes/all_html_top.html.php');
+		require_once('context.html.php');
+		require_once('../includes/all_html_bottom.html.php');
+		exit;
+	}
+}
+
+?>
